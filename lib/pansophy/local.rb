5302fdcecb591f039bad9bc3bea0e0df786859d1
Alessandro Berardi <berardialessandro@gmail.com>
2015-04-17 08:59:04 +0930

    Extracts local directory removal and creation

--- lib/pansophy/local.rb
+++ lib/pansophy/local.rb
@@ -12,11 +12,27 @@ module Pansophy
     end
     memoize :directory
 
+    def create(options)
+      remove(options)
+      directory.mkpath
+    end
+
     private
 
     def verify_directory!
       return if directory.directory? || !directory.exist?
       fail ArgumentError, "#{directory} is not a directory"
+    end
+
+    def remove(options)
+      return unless directory.exist?
+      prevent_overwrite! unless options[:overwrite]
+      directory.rmtree
+    end
+
+    def prevent_overwrite!
+      fail ArgumentError,
+           "#{directory} already exists, pass ':overwrite => true' to overwrite"
     end
   end
 end
--- lib/pansophy/synchronizer.rb
+++ lib/pansophy/synchronizer.rb
@@ -6,8 +6,7 @@ module Pansophy
     end
 
     def pull(options = {})
-      remove_target(options)
-
+      @local.create(options)
       @remote.files.each do |file|
         file_path = @local.directory.join(file.relative_to(@remote.directory_name))
         file_path.dirname.mkpath
@@ -15,17 +14,6 @@ module Pansophy
           f.write file.body
         end
       end
-    end
-
-    private
-
-    def remove_target(options)
-      return unless @local.directory.exist?
-      unless options[:overwrite]
-        fail ArgumentError,
-             "#{@local.directory} already exists, pass ':overwrite => true' to overwrite"
-      end
-      @local.directory.rmtree
     end
   end
 end
