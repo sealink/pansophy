471f9b44908b944ab68c556ac050c85064672479
Alessandro Berardi <berardialessandro@gmail.com>
2015-04-29 16:29:52 +0930

    Namespaces task accordingly to gem name

--- lib/pansophy/tasks.rb
+++ lib/pansophy/tasks.rb
@@ -1,10 +1,12 @@
-namespace :config do
-  desc 'Synchronize config files from remote folder'
-  task :synchronize do
-    require 'pansophy/config_synchronizer'
-    synchronizer = Pansophy::ConfigSynchronizer.new
-    puts "Fetching remote configuration (version #{synchronizer.version})"
-    synchronizer.merge
-    puts done
+namespace :pansophy do
+  namespace :config do
+    desc 'Synchronize config files from remote folder'
+    task :synchronize do
+      require 'pansophy/config_synchronizer'
+      synchronizer = Pansophy::ConfigSynchronizer.new
+      puts "Fetching remote configuration (version #{synchronizer.version})"
+      synchronizer.merge
+      puts done
+    end
   end
 end
