755bfcee9bb8f3db3d7e707a95f9fe2f32b09f48
Alessandro Berardi <berardialessandro@gmail.com>
2015-04-29 16:26:39 +0930

    Removes require referring to development dependency

--- lib/pansophy/tasks.rb
+++ lib/pansophy/tasks.rb
@@ -2,8 +2,6 @@ namespace :config do
   desc 'Synchronize config files from remote folder'
   task :synchronize do
     require 'pansophy/config_synchronizer'
-    require 'dotenv'
-    Dotenv.load
     synchronizer = Pansophy::ConfigSynchronizer.new
     puts "Fetching remote configuration (version #{synchronizer.version})"
     synchronizer.merge
