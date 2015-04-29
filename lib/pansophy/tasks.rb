ef4f779f969cde15c39410da06c3e50c03b4c8ae
Alessandro Berardi <berardialessandro@gmail.com>
2015-04-29 16:11:52 +0930

    Rubocop

--- lib/pansophy/tasks.rb
+++ lib/pansophy/tasks.rb
@@ -1,5 +1,5 @@
 namespace :config do
-  desc "Synchronize config files from remote folder"
+  desc 'Synchronize config files from remote folder'
   task :synchronize do
     require 'pansophy/config_synchronizer'
     require 'dotenv'
