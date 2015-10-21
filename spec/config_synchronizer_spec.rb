2674aa7e0c30a14abb8e26bc3a9fa1598d05f731
Alessandro Berardi <berardialessandro@gmail.com>
2015-10-21 10:42:54 +1030

    Cleans up spec and adds spec for empty attributes

--- spec/config_synchronizer_spec.rb
+++ spec/config_synchronizer_spec.rb
@@ -5,8 +5,12 @@ require 'pansophy'
 describe Pansophy::ConfigSynchronizer do
   subject(:config_synchronizer) { Pansophy::ConfigSynchronizer.new }
 
+  let(:synchronizer_class) { double(new: synchronizer) }
+  let(:synchronizer) { double }
+
   before do
-    allow(Pansophy).to receive(:merge)
+    stub_const('Pansophy::Synchronizer', synchronizer_class)
+    allow(synchronizer).to receive(:merge)
   end
 
   context 'when the configuration attributes are set explicitly' do
@@ -23,22 +27,27 @@ describe Pansophy::ConfigSynchronizer do
       end
 
       specify do
-        expect(Pansophy).to have_received(:merge).with(
-          'bucket',
-          'remote_config/1.5',
-          'local_config',
-          overwrite: true
-        )
+        expect(synchronizer_class)
+          .to have_received(:new).with('bucket', 'remote_config/1.5', 'local_config')
+      end
+
+      specify do
+        expect(synchronizer).to have_received(:merge).with(overwrite: true)
       end
     end
   end
 
   context 'when the configuration attributes are specified via environment variables' do
+    let(:config_bucket_name)    { 'bucket' }
+    let(:config_remote_folder)  { 'remote_config' }
+    let(:config_local_folder)   { 'local_config' }
+    let(:config_version)        { '1.5' }
+
     before do
-      ENV['CONFIG_BUCKET_NAME']   = 'bucket'
-      ENV['CONFIG_REMOTE_FOLDER'] = 'remote_config'
-      ENV['CONFIG_LOCAL_FOLDER']  = 'local_config'
-      ENV['CONFIG_VERSION']       = '1.5'
+      ENV['CONFIG_BUCKET_NAME']   = config_bucket_name
+      ENV['CONFIG_REMOTE_FOLDER'] = config_remote_folder
+      ENV['CONFIG_LOCAL_FOLDER']  = config_local_folder
+      ENV['CONFIG_VERSION']       = config_version
     end
 
     after do
@@ -53,13 +62,30 @@ describe Pansophy::ConfigSynchronizer do
         config_synchronizer.merge
       end
 
-      specify do
-        expect(Pansophy).to have_received(:merge).with(
-          'bucket',
-          'remote_config/1.5',
-          'local_config',
-          overwrite: true
-        )
+      context 'when all attributes are present' do
+        specify do
+          expect(synchronizer_class)
+            .to have_received(:new).with('bucket', 'remote_config/1.5', 'local_config')
+        end
+
+        specify do
+          expect(synchronizer).to have_received(:merge).with(overwrite: true)
+        end
+      end
+
+      context 'when some attributes are not present' do
+        let(:config_bucket_name)    { 'bucket' }
+        let(:config_remote_folder)  { '' }
+        let(:config_local_folder)   { 'local_config' }
+        let(:config_version)        { '' }
+
+        specify do
+          expect(synchronizer_class).to have_received(:new).with('bucket', '.', 'local_config')
+        end
+
+        specify do
+          expect(synchronizer).to have_received(:merge).with(overwrite: true)
+        end
       end
     end
   end
@@ -104,8 +130,12 @@ describe Pansophy::ConfigSynchronizer do
       end
 
       it 'should use default values' do
-        expect(Pansophy)
-          .to have_received(:merge).with('bucket', 'config/1.0', 'local_config', overwrite: true)
+        expect(synchronizer_class)
+          .to have_received(:new).with('bucket', 'config/1.0', 'local_config')
+      end
+
+      specify do
+        expect(synchronizer).to have_received(:merge).with(overwrite: true)
       end
     end
   end
@@ -124,12 +154,12 @@ describe Pansophy::ConfigSynchronizer do
       end
 
       it 'should use rails config folder as the local folder' do
-        expect(Pansophy).to have_received(:merge).with(
-          'bucket',
-          'config/1.0',
-          'rails/root/config',
-          overwrite: true
-        )
+        expect(synchronizer_class)
+          .to have_received(:new).with('bucket', 'config/1.0', 'rails/root/config')
+      end
+
+      specify do
+        expect(synchronizer).to have_received(:merge).with(overwrite: true)
       end
     end
 
@@ -142,12 +172,12 @@ describe Pansophy::ConfigSynchronizer do
       end
 
       it 'should use the sinatra config folder as the local folder' do
-        expect(Pansophy).to have_received(:merge).with(
-          'bucket',
-          'config/1.0',
-          'sinatra/root/config',
-          overwrite: true
-        )
+        expect(synchronizer_class)
+          .to have_received(:new).with('bucket', 'config/1.0', 'sinatra/root/config')
+      end
+
+      specify do
+        expect(synchronizer).to have_received(:merge).with(overwrite: true)
       end
     end
   end
