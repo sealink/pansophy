598ebd97a13cce342daeed86b3f30111deb1417b
John Mortlock <john.mortlock@gmail.com>
2017-07-26 15:00:12 +0930

    Allow all aws-s3 attributes to be set on the file

--- lib/pansophy/remote/create_file.rb
+++ lib/pansophy/remote/create_file.rb
@@ -3,6 +3,24 @@ module Pansophy
     class CreateFile
       include Adamantium::Flat
 
+      ALLOWED_ATTRS = %i[
+        cache_control
+        content_disposition
+        content_encoding
+        content_length
+        content_md5
+        content_type
+        etag
+        expires
+        last_modified
+        metadata
+        owner
+        storage_class
+        encryption
+        encryption_key
+        version
+      ].freeze
+
       def initialize(bucket, path, body)
         @bucket   = bucket
         @pathname = Pathname.new(path)
@@ -11,7 +29,8 @@ module Pansophy
 
       def call(options = {})
         prevent_overwrite! unless options[:overwrite]
-        directory.files.create(key: @pathname.to_s, body: @body.dup)
+        params = options.slice(*ALLOWED_ATTRS).merge(key: @pathname.to_s, body: @body.dup)
+        directory.files.create(params)
       end
 
       private
